522ef5ffffd2c97792e7854d90eaf8843693d9fa
Ayoub-Elmamoun <saadcrax@protonmail.com>
2022-06-21 20:33:13 +0100

    added

--- Realisation/public/index.php
+++ Realisation/public/index.php
@@ -3,10 +3,10 @@
 <head>
  <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-<meta content="Themesdesign" name="author" />
+<
 <!-- App favicon -->
 <link rel="shortcut icon" href="assets/images/favicon.ico">
+
 
  <title>MFE - Make Freelancing Easy</title>
 </head>
