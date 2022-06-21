a1e72af6ce6a79c2c71dc21788a23c0de9c17ed1
Ayoub-Elmamoun <saadcrax@protonmail.com>
2022-06-21 20:28:43 +0100

    ddeed

--- Realisation/public/index.php
+++ Realisation/public/index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="asset/style/style.css">
-    <link rel="stylesheet" href="asset/style/flexboxgrid.css">
-    <link rel='stylesheet' href="asset/style/responsive.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>MFE - Make Freelancing Easy</title>
+ <meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+<meta content="Themesdesign" name="author" />
+<!-- App favicon -->
+<link rel="shortcut icon" href="assets/images/favicon.ico">
+
+ <title>MFE - Make Freelancing Easy</title>
 </head>
 <body>
     <header>
